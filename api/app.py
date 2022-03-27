eb3f0169eedeb5ac935ac94502506c3250a949ae
tinou_99 <tinou99.f@gmail.com>
2022-03-27 23:50:28 +0200

    chg: web app modified

--- api/app.py
+++ api/app.py
@@ -1,27 +1,31 @@
-import pandas as pd
 from flask import Flask, render_template, request
-from queries import *
-from get_data_to_rdf import get_data_to_rdf_object, get_data_to_rdf_train_station
-
+from queries import query_get_last_date_of_lost_objects, query_get_all_nature, query_get_lost_object_with_conditions, \
+    query_get_lat_long_name_train_station, query_get_all_lost_object_with_conditions
+from get_data_to_rdf import get_data_to_rdf_object
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home_app():
-    input_path_owl_file = 'C:/Users/tinou/PycharmProjects/web_datamining_and_semantics/data/train_station_context.owl'
-    path_owl_file = 'C:/Users/tinou/PycharmProjects/web_datamining_and_semantics/data/output_context.owl'
+    input_path_owl_file = '../data/train_station_context.owl'
+    path_owl_file = '../data/output_context.owl'
     get_data_to_rdf_object(input_path_owl_file, path_owl_file)
     last = query_get_last_date_of_lost_objects(path_owl_file)
-    last_date = last[0]
-    last_place = last[1]
+    last_object = last[0]
+    last_date = last[1]
+    last_place = last[2]
+
+    markers = query_get_lat_long_name_train_station(path_owl_file)
 
     return render_template('home.html',
                            l_nature_windows=query_get_all_nature(path_owl_file),
                            last_date=last_date[:10],
                            last_hour=((last_date.split('T')[1])[:8]),
-                           last_place=last_place
-           )
+                           last_place=last_place,
+                           last_object=last_object,
+                           markers=markers
+                           )
 
 
 @app.route('/result/', methods=['GET', 'POST'])
@@ -36,18 +40,14 @@ def result_page():
         b_recovered_date = request.form.get('rdate_selected')
         print(f'recovered_date: {b_recovered_date}')
 
-        if nature_obj != '' and zipcode != '':
+        if nature_obj == 'All' and zipcode != '':
+            query_final = query_get_all_lost_object_with_conditions(path_owl_file, zipcode, hasRecoveredDate)
+        elif nature_obj != 'All' and zipcode != '':
             query_final = query_get_lost_object_with_conditions(path_owl_file, nature_obj, zipcode, hasRecoveredDate)
-            print('only nature')
-        elif nature_obj == '' and zipcode != '':
-            query_final = query_get_lost_object_with_conditions(path_owl_file, 'nature', zipcode, hasRecoveredDate)
-            print('only nature')
-        elif nature_obj != '' and zipcode == '':
-            query_final = query_get_lost_object_with_conditions(path_owl_file, nature_obj, 'zipcode', hasRecoveredDate)
-            print('only zipcode')
+        elif nature_obj == 'All' and zipcode == '':
+            query_final = query_get_all_lost_object_with_conditions(path_owl_file, 'zipcode', hasRecoveredDate)
         else:
-            query_final = query_get_lost_object_with_conditions(path_owl_file, 'nature', 'zipcode', hasRecoveredDate)
-            print('both')
+            query_final = query_get_lost_object_with_conditions(path_owl_file, nature_obj, 'zipcode', hasRecoveredDate)
         verif_df = len(query_final)
         return render_template('result.html', df_result=[query_final.to_html(classes='d')], len_df=verif_df)
 
